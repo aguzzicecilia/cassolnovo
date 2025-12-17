cb417ce3c465e32634e20ce9aa2493db88594c69
aguzzicecilia <aguzzi.cecilia@gmail.com>
2025-12-17 09:42:52 +0100

    wp-content

---
+++ wp-content/manifest.js
@@ -0,0 +1,623 @@
+// File generato automaticamente. NON modificare a mano.
+window.ASSETS_MANIFEST = [
+  {
+    "file": "|Aguzzi_Giuseppe|persone-uomo.jpg",
+    "name": "Aguzzi_Giuseppe|persone-uomo"
+  },
+  {
+    "file": "|Asilo_d'infanzia|paesaggi-cartoline.jpg",
+    "name": "Asilo_d'infanzia|paesaggi-cartoline"
+  },
+  {
+    "file": "|Bambini_che_giocano|persone-bambini.jpg",
+    "name": "Bambini_che_giocano|persone-bambini"
+  },
+  {
+    "file": "|Bambini_si_riposano|persone-bambini.jpg",
+    "name": "Bambini_si_riposano|persone-bambini"
+  },
+  {
+    "file": "|Bambino_alla_finestra_|persone-bambini.jpg",
+    "name": "Bambino_alla_finestra_|persone-bambini"
+  },
+  {
+    "file": "|Bambino_in_braccio_alla_sorella|persone-bambini.jpg",
+    "name": "Bambino_in_braccio_alla_sorella|persone-bambini"
+  },
+  {
+    "file": "|Bambino_in_cortile|persone-bambini.jpg",
+    "name": "Bambino_in_cortile|persone-bambini"
+  },
+  {
+    "file": "|Bambino_su_tricilo,_1918|persone-bambini.jpg",
+    "name": "Bambino_su_tricilo,_1918|persone-bambini"
+  },
+  {
+    "file": "|Bambino_sul_fasciatoio|persone-bambini.jpg",
+    "name": "Bambino_sul_fasciatoio|persone-bambini"
+  },
+  {
+    "file": "|Bambino_vicino_alla_sedia|persone-bambini.jpg",
+    "name": "Bambino_vicino_alla_sedia|persone-bambini"
+  },
+  {
+    "file": "|Bambino|persone-bambini.jpg",
+    "name": "Bambino|persone-bambini"
+  },
+  {
+    "file": "|Bandi_Maria_Giuseppina|persone-donna.jpg",
+    "name": "Bandi_Maria_Giuseppina|persone-donna"
+  },
+  {
+    "file": "|Bersagliere|persone-uomo.jpg",
+    "name": "Bersagliere|persone-uomo"
+  },
+  {
+    "file": "|Bombardiere_Lancaster_Canadese_abbattuto_1|-paesaggi.jpg",
+    "name": "Bombardiere_Lancaster_Canadese_abbattuto_1|-paesaggi"
+  },
+  {
+    "file": "|Bombardiere_Lancaster_Canadese_abbattuto_2|paesaggi.jpg",
+    "name": "Bombardiere_Lancaster_Canadese_abbattuto_2|paesaggi"
+  },
+  {
+    "file": "|Carabiniere|persone-uomo.jpg",
+    "name": "Carabiniere|persone-uomo"
+  },
+  {
+    "file": "|Carolina_e_Giuseppe-Oggi_sposi,_1904|persone-gruppi.jpg",
+    "name": "Carolina_e_Giuseppe-Oggi_sposi,_1904|persone-gruppi"
+  },
+  {
+    "file": "|Cartolina_azienda_vini|paesaggi-cartoline.jpg",
+    "name": "Cartolina_azienda_vini|paesaggi-cartoline"
+  },
+  {
+    "file": "|Casa_Arconati|paesaggi-cartoline.jpg",
+    "name": "Casa_Arconati|paesaggi-cartoline"
+  },
+  {
+    "file": "|Cassolnovo_panorama_(Verso_San_Bartolomeo)|paesaggi-cartoline.jpg",
+    "name": "Cassolnovo_panorama_(Verso_San_Bartolomeo)|paesaggi-cartoline"
+  },
+  {
+    "file": "|Cassolnovo_panorama_(Verso_San_Giorgio)|paesaggi-cartoline.jpg",
+    "name": "Cassolnovo_panorama_(Verso_San_Giorgio)|paesaggi-cartoline"
+  },
+  {
+    "file": "|Cassolnovo|paesaggi-cartoline.jpg",
+    "name": "Cassolnovo|paesaggi-cartoline"
+  },
+  {
+    "file": "|Chiesa_Parrocchiale|paesaggi-cartoline.jpg",
+    "name": "Chiesa_Parrocchiale|paesaggi-cartoline"
+  },
+  {
+    "file": "|Chiesa_di_San_Bartolomeo|paesaggi-cartoline.jpg",
+    "name": "Chiesa_di_San_Bartolomeo|paesaggi-cartoline"
+  },
+  {
+    "file": "|Chiesa_di_San_Giacomo,_1871|paesaggi.jpg",
+    "name": "Chiesa_di_San_Giacomo,_1871|paesaggi"
+  },
+  {
+    "file": "|Chiesa_di_San_Giorgio, 1964|paesaggi-cartoline.jpg",
+    "name": "Chiesa_di_San_Giorgio, 1964|paesaggi-cartoline"
+  },
+  {
+    "file": "|Chiesa_di_San_Giorgio|paesaggi-cartoline.jpg",
+    "name": "Chiesa_di_San_Giorgio|paesaggi-cartoline"
+  },
+  {
+    "file": "|Colonia_fluviale|paesaggi-cartoline.jpg",
+    "name": "Colonia_fluviale|paesaggi-cartoline"
+  },
+  {
+    "file": "|Convitto|paesaggi-cartoline.jpg",
+    "name": "Convitto|paesaggi-cartoline"
+  },
+  {
+    "file": "|Coppia| persone-gruppi.jpg",
+    "name": "Coppia| persone-gruppi"
+  },
+  {
+    "file": "|Corso_Carlo_Alberto_2|paesaggi-cartoline.jpg",
+    "name": "Corso_Carlo_Alberto_2|paesaggi-cartoline"
+  },
+  {
+    "file": "|Donna_anziana_1|persone-donna.jpg",
+    "name": "Donna_anziana_1|persone-donna"
+  },
+  {
+    "file": "|Donna_anziana_2|persone-donna.jpg",
+    "name": "Donna_anziana_2|persone-donna"
+  },
+  {
+    "file": "|Donna_anziana_3|persone-donna.jpg",
+    "name": "Donna_anziana_3|persone-donna"
+  },
+  {
+    "file": "|Donna_anziana_4|persone-donna.jpg",
+    "name": "Donna_anziana_4|persone-donna"
+  },
+  {
+    "file": "|Donna_anziana|persone-donna.jpg",
+    "name": "Donna_anziana|persone-donna"
+  },
+  {
+    "file": "|Donna_che_suona_il_pianoforte|persone-donna.jpg",
+    "name": "Donna_che_suona_il_pianoforte|persone-donna"
+  },
+  {
+    "file": "|Donna_con_il_cappello|persone-donna.jpg",
+    "name": "Donna_con_il_cappello|persone-donna"
+  },
+  {
+    "file": "|Donna_di_profilo|persone-donna.jpg",
+    "name": "Donna_di_profilo|persone-donna"
+  },
+  {
+    "file": "|Donna_di_spalle|persone-donna.jpg",
+    "name": "Donna_di_spalle|persone-donna"
+  },
+  {
+    "file": "|Donna_e_bambino|persone-donna.jpg",
+    "name": "Donna_e_bambino|persone-donna"
+  },
+  {
+    "file": "|Donna_in_divisa|persone-donna.jpg",
+    "name": "Donna_in_divisa|persone-donna"
+  },
+  {
+    "file": "|Donna_in_un_campo|persone-donna.jpg",
+    "name": "Donna_in_un_campo|persone-donna"
+  },
+  {
+    "file": "|Donna_nel_prato|persone-donna.jpg",
+    "name": "Donna_nel_prato|persone-donna"
+  },
+  {
+    "file": "|Donna_sorridente|persone-donna.jpg",
+    "name": "Donna_sorridente|persone-donna"
+  },
+  {
+    "file": "|Donna|persone-donna.jpg",
+    "name": "Donna|persone-donna"
+  },
+  {
+    "file": "|Donne_e_bambina_in_cortile|persone-donna.jpg",
+    "name": "Donne_e_bambina_in_cortile|persone-donna"
+  },
+  {
+    "file": "|Dopolavoro|persone-gruppi.jpg",
+    "name": "Dopolavoro|persone-gruppi"
+  },
+  {
+    "file": "|Eucarestia|persone-gruppi.jpg",
+    "name": "Eucarestia|persone-gruppi"
+  },
+  {
+    "file": "|Facciata_Chiesa_San_Giorgio|paesaggi-cartoline.jpg",
+    "name": "Facciata_Chiesa_San_Giorgio|paesaggi-cartoline"
+  },
+  {
+    "file": "|Famiglia_(sconosciuti)|persone-gruppi.jpg",
+    "name": "Famiglia_(sconosciuti)|persone-gruppi"
+  },
+  {
+    "file": "|Famiglia_Madre_e_figli|persone-bambini.jpg",
+    "name": "Famiglia_Madre_e_figli|persone-bambini"
+  },
+  {
+    "file": "|Famiglia_in_cortile|persone-gruppi.jpg",
+    "name": "Famiglia_in_cortile|persone-gruppi"
+  },
+  {
+    "file": "|Fontana_(Piazza_del_Comune),_1959|paesaggi-cartoline.jpg",
+    "name": "Fontana_(Piazza_del_Comune),_1959|paesaggi-cartoline"
+  },
+  {
+    "file": "|Foto_del_regime_davanti_al_comune| persone-gruppi.jpg",
+    "name": "Foto_del_regime_davanti_al_comune| persone-gruppi"
+  },
+  {
+    "file": "|Foto_del_regime| persone-gruppi.jpg",
+    "name": "Foto_del_regime| persone-gruppi"
+  },
+  {
+    "file": "|Foto_di_gruppo|uomo-gruppi.jpg",
+    "name": "Foto_di_gruppo|uomo-gruppi"
+  },
+  {
+    "file": "|Foto_rovinata_di_un_uomo|persone-uomo.jpg",
+    "name": "Foto_rovinata_di_un_uomo|persone-uomo"
+  },
+  {
+    "file": "|Foto_tessere_di_donna|persone-donna.jpg",
+    "name": "Foto_tessere_di_donna|persone-donna"
+  },
+  {
+    "file": "|Fototessare_di_un_uomo|persone-uomo.jpg",
+    "name": "Fototessare_di_un_uomo|persone-uomo"
+  },
+  {
+    "file": "|Fototessera_di_un_uomo_in_divisa|persone-uomo.jpg",
+    "name": "Fototessera_di_un_uomo_in_divisa|persone-uomo"
+  },
+  {
+    "file": "|Fototessera_di_un_uomo_in_giacca_e_cravatta|persone-uomo.jpg",
+    "name": "Fototessera_di_un_uomo_in_giacca_e_cravatta|persone-uomo"
+  },
+  {
+    "file": "|Fototessera_donna_anziana|persone-donna.jpg",
+    "name": "Fototessera_donna_anziana|persone-donna"
+  },
+  {
+    "file": "|Giovane_donna_su_set|persone-donna.jpg",
+    "name": "Giovane_donna_su_set|persone-donna"
+  },
+  {
+    "file": "|Gruppo_di_giovani_uomini|persone-gruppi.jpg",
+    "name": "Gruppo_di_giovani_uomini|persone-gruppi"
+  },
+  {
+    "file": "|Gruppo_di_miliatri|persone-gruppi.jpg",
+    "name": "Gruppo_di_miliatri|persone-gruppi"
+  },
+  {
+    "file": "|Laviatoio_via_Montebello|paesaggi-cartoline.jpg",
+    "name": "Laviatoio_via_Montebello|paesaggi-cartoline"
+  },
+  {
+    "file": "|Marito_e_moglie_in_cortile|persone-gruppi.jpg",
+    "name": "Marito_e_moglie_in_cortile|persone-gruppi"
+  },
+  {
+    "file": "|Mesto_ricordo_di_Pisi_Corina|persone-donna.jpg",
+    "name": "Mesto_ricordo_di_Pisi_Corina|persone-donna"
+  },
+  {
+    "file": "|Militare_davanti_alle_piramidi|persone-uomo.jpg",
+    "name": "Militare_davanti_alle_piramidi|persone-uomo"
+  },
+  {
+    "file": "|Militare_in_una_grotta|persone-uomo.jpg",
+    "name": "Militare_in_una_grotta|persone-uomo"
+  },
+  {
+    "file": "|Mobili_Sanpietro|paesaggi.jpg",
+    "name": "Mobili_Sanpietro|paesaggi"
+  },
+  {
+    "file": "|Molino_del_conte|paesaggi-cartoline.jpg",
+    "name": "Molino_del_conte|paesaggi-cartoline"
+  },
+  {
+    "file": "|Molino_del_conte|paesaggi.jpg",
+    "name": "Molino_del_conte|paesaggi"
+  },
+  {
+    "file": "|Mondine_1|persone-gruppi.jpg",
+    "name": "Mondine_1|persone-gruppi"
+  },
+  {
+    "file": "|Mondine_2|persone-gruppi.jpg",
+    "name": "Mondine_2|persone-gruppi"
+  },
+  {
+    "file": "|Mondine_3|persone-gruppi.jpg",
+    "name": "Mondine_3|persone-gruppi"
+  },
+  {
+    "file": "|Mondine_4|persone-gruppi.jpg",
+    "name": "Mondine_4|persone-gruppi"
+  },
+  {
+    "file": "|Municipio_e_Casa_del_Facio|paesaggi-cartoline.jpg",
+    "name": "Municipio_e_Casa_del_Facio|paesaggi-cartoline"
+  },
+  {
+    "file": "|Municipio|paesaggi-cartoline.jpg",
+    "name": "Municipio|paesaggi-cartoline"
+  },
+  {
+    "file": "|Naviglio_Langosco,_1950|paesaggi-cartoline.jpg",
+    "name": "Naviglio_Langosco,_1950|paesaggi-cartoline"
+  },
+  {
+    "file": "|Naviglio_Langosco,_1953|paesaggi-cartoline.jpg",
+    "name": "Naviglio_Langosco,_1953|paesaggi-cartoline"
+  },
+  {
+    "file": "|Naviglio_Langosco_2,_1950|paesaggi-cartoline.jpg",
+    "name": "Naviglio_Langosco_2,_1950|paesaggi-cartoline"
+  },
+  {
+    "file": "|Naviglio_Langosco|paesaggi-cartoline.jpg",
+    "name": "Naviglio_Langosco|paesaggi-cartoline"
+  },
+  {
+    "file": "|Piazza_Costa|paesaggi-cartoline.jpg",
+    "name": "Piazza_Costa|paesaggi-cartoline"
+  },
+  {
+    "file": "|Piazza_Vittorio_Emanuele_II,_attuale_Via_Lavetelli|paesaggi-cartoline.jpg",
+    "name": "Piazza_Vittorio_Emanuele_II,_attuale_Via_Lavetelli|paesaggi-cartoline"
+  },
+  {
+    "file": "|Ragazza_davanti_alla_statua_di_Don_Bosco|persone-donna.jpg",
+    "name": "Ragazza_davanti_alla_statua_di_Don_Bosco|persone-donna"
+  },
+  {
+    "file": "|Ragazza_e_uva|persone-donna.jpg",
+    "name": "Ragazza_e_uva|persone-donna"
+  },
+  {
+    "file": "|Ragazza_fototessera|persone-donna.jpg",
+    "name": "Ragazza_fototessera|persone-donna"
+  },
+  {
+    "file": "|Ragazza_in_cortile|persone-donna.jpg",
+    "name": "Ragazza_in_cortile|persone-donna"
+  },
+  {
+    "file": "|Ragazza_sulla_porta_(sconosciuta)|donna.jpg",
+    "name": "Ragazza_sulla_porta_(sconosciuta)|donna"
+  },
+  {
+    "file": "|Ragazza|persone-donna.jpg",
+    "name": "Ragazza|persone-donna"
+  },
+  {
+    "file": "|Ragazze_con_lo_stess_abito_in_cortile_|persone-donna.jpg",
+    "name": "Ragazze_con_lo_stess_abito_in_cortile_|persone-donna"
+  },
+  {
+    "file": "|Ragazze_in_campagna|persone-donna.jpg",
+    "name": "Ragazze_in_campagna|persone-donna"
+  },
+  {
+    "file": "|Ragazze_in_cortile|persone-donna.jpg",
+    "name": "Ragazze_in_cortile|persone-donna"
+  },
+  {
+    "file": "|Ragazze_sul_set|persone-donna.jpg",
+    "name": "Ragazze_sul_set|persone-donna"
+  },
+  {
+    "file": "|Ragazzo_con_maglione_con_fantasie|persone-uomo.jpg",
+    "name": "Ragazzo_con_maglione_con_fantasie|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_di_profilo|persone-uomo.jpg",
+    "name": "Ragazzo_di_profilo|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_elegante|persone-uomo.jpg",
+    "name": "Ragazzo_elegante|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_in posa|persone-uomo.jpg",
+    "name": "Ragazzo_in posa|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_in_cortile|persone-uomo.jpg",
+    "name": "Ragazzo_in_cortile|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_in_posa_sul_set|persone-uomo.jpg",
+    "name": "Ragazzo_in_posa_sul_set|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo_legge_il_giornale_in_cortile|persone-uomo.jpg",
+    "name": "Ragazzo_legge_il_giornale_in_cortile|persone-uomo"
+  },
+  {
+    "file": "|Ragazzo|persone-uomo.jpg",
+    "name": "Ragazzo|persone-uomo"
+  },
+  {
+    "file": "|Ritratto_di_una_donna_|persone-donna.jpg",
+    "name": "Ritratto_di_una_donna_|persone-donna"
+  },
+  {
+    "file": "|Saluti_da_Cassolnovo|cartoline.jpg",
+    "name": "Saluti_da_Cassolnovo|cartoline"
+  },
+  {
+    "file": "|San_Bartolomeo|paesaggi.jpg",
+    "name": "San_Bartolomeo|paesaggi"
+  },
+  {
+    "file": "|Scuole_elementari|paesaggi-cartoline.jpg",
+    "name": "Scuole_elementari|paesaggi-cartoline"
+  },
+  {
+    "file": "|Signora|persone-donna.jpg",
+    "name": "Signora|persone-donna"
+  },
+  {
+    "file": "|Ticino|paesaggi.jpg",
+    "name": "Ticino|paesaggi"
+  },
+  {
+    "file": "|Ultima_sera_di_carnevale,_1827|persone-gruppi.jpg",
+    "name": "Ultima_sera_di_carnevale,_1827|persone-gruppi"
+  },
+  {
+    "file": "|Uomini_a_ticino| persone-gruppi.jpg",
+    "name": "Uomini_a_ticino| persone-gruppi"
+  },
+  {
+    "file": "|Uomo_anizano|persone-uomo.jpg",
+    "name": "Uomo_anizano|persone-uomo"
+  },
+  {
+    "file": "|Uomo_anziano_(sconosciuto)|uomo.jpg",
+    "name": "Uomo_anziano_(sconosciuto)|uomo"
+  },
+  {
+    "file": "|Uomo_anziano_con_i_baffi|persone-uomo.jpg",
+    "name": "Uomo_anziano_con_i_baffi|persone-uomo"
+  },
+  {
+    "file": "|Uomo_anziano_in_divisa_(sconosciuto)|uomo.jpg",
+    "name": "Uomo_anziano_in_divisa_(sconosciuto)|uomo"
+  },
+  {
+    "file": "|Uomo_calvo_in_divisa|persone-uomo.jpg",
+    "name": "Uomo_calvo_in_divisa|persone-uomo"
+  },
+  {
+    "file": "|Uomo_che_legge_il_giornale_(sconosciuto)|uomo.jpg",
+    "name": "Uomo_che_legge_il_giornale_(sconosciuto)|uomo"
+  },
+  {
+    "file": "|Uomo_elegante|persone-uomo.jpg",
+    "name": "Uomo_elegante|persone-uomo"
+  },
+  {
+    "file": "|Uomo_foto_tessera_(sconosciuto)|uomo.jpg",
+    "name": "Uomo_foto_tessera_(sconosciuto)|uomo"
+  },
+  {
+    "file": "|Uomo_fototessera|persone-uomo.jpg",
+    "name": "Uomo_fototessera|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_cortile|persone-uomo.jpg",
+    "name": "Uomo_in_cortile|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_divisa_1|persone-uomo.jpg",
+    "name": "Uomo_in_divisa_1|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_divisa_2|persone-uomo.jpg",
+    "name": "Uomo_in_divisa_2|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_divisa_davanti_ad_un_negozio|persone-uomo.jpg",
+    "name": "Uomo_in_divisa_davanti_ad_un_negozio|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_divisa_e_armato|uomo.jpg",
+    "name": "Uomo_in_divisa_e_armato|uomo"
+  },
+  {
+    "file": "|Uomo_in_divisa_e_in_cortile|uomo.jpg",
+    "name": "Uomo_in_divisa_e_in_cortile|uomo"
+  },
+  {
+    "file": "|Uomo_in_giacca_e_cravatta|persone-uomo.jpg",
+    "name": "Uomo_in_giacca_e_cravatta|persone-uomo"
+  },
+  {
+    "file": "|Uomo_in_posa|persone-uomo.jpg",
+    "name": "Uomo_in_posa|persone-uomo"
+  },
+  {
+    "file": "|Uomo_nel_deserto|persone-uomo.jpg",
+    "name": "Uomo_nel_deserto|persone-uomo"
+  },
+  {
+    "file": "|Via_Carlo_Alberto,_1871|paesaggi.jpg",
+    "name": "Via_Carlo_Alberto,_1871|paesaggi"
+  },
+  {
+    "file": "|Via_Carlo_Alberto,_1910-15|paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto,_1910-15|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto_1|-paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto_1|-paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto_2|-paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto_2|-paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto_2|paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto_2|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto_3|-paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto_3|-paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto_3|paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto_3|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Carlo_Alberto|paesaggi-cartoline.jpg",
+    "name": "Via_Carlo_Alberto|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Cavour|paesaggi-cartoline.jpg",
+    "name": "Via_Cavour|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Coniugi_Lavatelli|paesaggi-cartoline.jpg",
+    "name": "Via_Coniugi_Lavatelli|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Lina_Lavatelli|paesaggi-cartoline.jpg",
+    "name": "Via_Lina_Lavatelli|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Modello,_1963|paesaggi-cartoline.jpg",
+    "name": "Via_Modello,_1963|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Roma_Ristorante_Bellaria|paesaggi-cartoline.jpg",
+    "name": "Via_Roma_Ristorante_Bellaria|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Roma_con_automobile|paesaggi-cartoline.jpg",
+    "name": "Via_Roma_con_automobile|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Roma|paesaggi-cartoline.jpg",
+    "name": "Via_Roma|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_San_Giorgio,_1933|paesaggi-cartoline.jpg",
+    "name": "Via_San_Giorgio,_1933|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_San_Giorgio,_1950|paesaggi-cartoline.jpg",
+    "name": "Via_San_Giorgio,_1950|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_San_Giorgio,_1962|paesaggi-cartoline.jpg",
+    "name": "Via_San_Giorgio,_1962|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_San_Giorgio,|paesaggi-cartoline.jpg",
+    "name": "Via_San_Giorgio,|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_San_Giorgio_verso_il_ricovero,_1871|paesaggi.jpg",
+    "name": "Via_San_Giorgio_verso_il_ricovero,_1871|paesaggi"
+  },
+  {
+    "file": "|Via_San_Giorgio|paesaggi-cartoline.jpg",
+    "name": "Via_San_Giorgio|paesaggi-cartoline"
+  },
+  {
+    "file": "|Via_Vittorio_Emanuele_II_ora_via_lavatelli,_1871|paesaggi.jpg",
+    "name": "Via_Vittorio_Emanuele_II_ora_via_lavatelli,_1871|paesaggi"
+  },
+  {
+    "file": "|Via_Vittorio_Emanuele_II|paesaggi-cartoline.jpg",
+    "name": "Via_Vittorio_Emanuele_II|paesaggi-cartoline"
+  },
+  {
+    "file": "|Viale_Rimembranze,_1950|paesaggi-cartoline.jpg",
+    "name": "Viale_Rimembranze,_1950|paesaggi-cartoline"
+  },
+  {
+    "file": "|Vignetta_di_un_ragazzo_in_divisa|persone-uomo.jpg",
+    "name": "Vignetta_di_un_ragazzo_in_divisa|persone-uomo"
+  }
+];
